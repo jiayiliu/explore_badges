7b5751a080de68b4ba2082d3aba3c77a44572af7
Jiayi (Jason) Liu <jiayi.uiuc@gmail.com>
2019-08-22 13:55:52 -0700

    update coverage report with python3 only

--- publish.sh
+++ publish.sh
@@ -2,5 +2,14 @@
 
 set -e 
 
+flag=$(python -c 'import platform; major, minor, patch = platform.python_version_tuple();print(major=="3" and minor=="7")')
+
+if [ $flag == "False" ]; then
+  exit 0
+fi
+
+codecov
+
 cd docs
+pandoc ../README.md -o source/README.rst
 make html
